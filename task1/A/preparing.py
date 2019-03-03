4c548beb7c8c4156de12f21c36781e5d805cac9f
uslon <mennibaev.as@phystech.edu>
2019-03-04 02:23:29 +0300

    First attempt

---
+++ task1/A/preparing.py
@@ -0,0 +1,2 @@
+f = open("index.h", "w")
+#f = open("~/Documents/PT/ProgrammingTechnologies/task1/A/index.h", "x")
---
+++ task1/C/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+	//ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	cout << "Fast input and output!\n";
+}
