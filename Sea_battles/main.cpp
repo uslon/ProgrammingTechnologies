0082de9b3381d6b71c919a913874969c32ada5b8
uslon <mennibaev.as@phystech.edu>
2019-05-09 10:33:00 +0300

    Sample SFML window

--- Sea_battles/main.cpp
+++ Sea_battles/main.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
+
 #include "Objects/Ships_classes/Ships.h"
 #include "Objects/Buildings_classes/Buildings.h"
 
+#include <SFML/Window.hpp>
 #include "googletest-master/googletest/include/gtest/gtest.h"
+#include "SFML-2.5.1/include/SFML/Graphics.hpp"
 
 using std::cin;
 using std::cout;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    Japanese_building_factory dock;
-    auto Tsar_pushka = dock.create_gun();
-    Building * b = Tsar_pushka;
-    testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
+
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
